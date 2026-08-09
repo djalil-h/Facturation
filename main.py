ed5d3203aa11a9ed3b383d8a88fb8b835d26623d
djalil-h <hamzaouiidjll@gmail.com>
2026-08-09 17:53:15 +0100

    Activer la sauvegarde automatique au démarrage

--- main.py
+++ main.py
@@ -1,22 +1,19 @@
 from database import create_database
+from services.backup_service import create_startup_backup
 from services.user_service import create_admin
 
 from UI.app import FacturationApp
 
 
 def main():
+    # Sauvegarder la base existante avant toute création/migration de schéma.
+    create_startup_backup()
 
     create_database()
-
     create_admin()
 
     app = FacturationApp()
-
-    app.protocol(
-        "WM_DELETE_WINDOW",
-        app.on_close
-    )
-
+    app.protocol("WM_DELETE_WINDOW", app.on_close)
     app.mainloop()
 
 
